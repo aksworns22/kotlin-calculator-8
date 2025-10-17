6e87799321bef430c28f682a697633608fb711fe
jangdaehan <wkdeogks17@gmail.com>
2025-10-18 00:20:41 +0900

    test: 기본 제공받은 테스트의 주석 처리를 해제

--- src/test/kotlin/calculator/ApplicationTest.kt
+++ src/test/kotlin/calculator/ApplicationTest.kt
@@ -118,20 +118,20 @@ class ApplicationTest : NsTest() {
             assertThrows<IllegalArgumentException> { PositiveNumbers(listOf(Int.MAX_VALUE, 1)).sum() }
         }
     }
-//    @Test
-//    fun `커스텀 구분자 사용`() {
-//        assertSimpleTest {
-//            run("//;\\n1")
-//            assertThat(output()).contains("결과 : 1")
-//        }
-//    }
-//
-//    @Test
-//    fun `예외 테스트`() {
-//        assertSimpleTest {
-//            assertThrows<IllegalArgumentException> { runException("-1,2,3") }
-//        }
-//    }
+    @Test
+    fun `커스텀 구분자 사용`() {
+        assertSimpleTest {
+            run("//;\\n1")
+            assertThat(output()).contains("결과 : 1")
+        }
+    }
+
+    @Test
+    fun `예외 테스트`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("-1,2,3") }
+        }
+    }
 
     override fun runMain() {
         main()
